e69247cdf8f86001721015f34541dd240b8bb0f6
arjunbhasin2013 <arjunbhasin2013@gmail.com>
2021-01-14 22:25:49 +0530

    Kafka Producer-Consumer Implementation (Local Hosted)

---
+++ Kafka/Kafka-Samp/consumer.py
@@ -0,0 +1,22 @@
+'''
+initial consumer 
+'''
+
+from kafka import KafkaConsumer
+from json import loads
+
+#consumer object
+consumer = KafkaConsumer(
+    'numtest',
+    bootstrap_servers = ['localhost:9092'],
+    auto_offset_reset = 'earliest',
+    enable_auto_commit = True,
+    group_id = 'my-group',
+    value_deserializer = lambda x: loads(x.decode('utf-8'))
+)
+
+#print consumer message
+for message in consumer:
+    print('Traversed:',message)
+
+
